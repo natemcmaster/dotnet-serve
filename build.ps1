1da60846c63c61fdc583bd394a0103c13e1b09b8
Nate McMaster <nate.mcmaster@gmail.com>
2018-10-17 19:41:56 -0700

    Implement authenticode signing and nuget package signing

--- build.ps1
+++ build.ps1
@@ -3,12 +3,19 @@
 param(
     [ValidateSet('Debug', 'Release')]
     $Configuration = $null,
+    [switch]
+    $ci,
 	[switch]
-	$IsOfficialBuild
+	$sign
 )
 
 Set-StrictMode -Version 1
 $ErrorActionPreference = 'Stop'
+
+if (-not (Test-Path Variable:/IsCoreCLR)) {
+    $IsCoreCLR = $false
+    $IsWindows = $true
+}
 
 function exec([string]$_cmd) {
     write-host -ForegroundColor DarkGray ">>> $_cmd $args"
@@ -25,15 +32,48 @@ function exec([string]$_cmd) {
 # Main
 #
 
+if ($env:CI -eq 'true') {
+    $ci = $true
+}
 
 if (!$Configuration) {
-    $Configuration = if ($env:CI -or $IsOfficialBuild) { 'Release' } else { 'Debug' }
+    $Configuration = if ($ci) { 'Release' } else { 'Debug' }
 }
 
 [string[]] $MSBuildArgs = @("-p:Configuration=$Configuration")
 
-if ($IsOfficialBuild) {
-	$MSBuildArgs += '-p:CI=true'
+if ($ci) {
+    $MSBuildArgs += '-p:CI=true'
+}
+
+$CodeSign = $sign -or ($ci -and -not $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -and $IsWindows)
+
+if ($CodeSign) {
+    $toolsDir = "$PSScriptRoot/.build/tools"
+
+    $AzureSignToolPath = "$toolsDir/azuresigntool"
+    if ($IsWindows) {
+        $AzureSignToolPath += ".exe"
+    }
+
+    if (-not (Test-Path $AzureSignToolPath)) {
+        exec dotnet tool install --tool-path $toolsDir `
+        AzureSignTool `
+        --version 2.0.17
+    }
+
+    $nstDir = "$toolsDir/nugetsigntool/1.1.4"
+    $NuGetKeyVaultSignToolPath = "$nstDir/tools/net471/NuGetKeyVaultSignTool.exe"
+    if (-not (Test-Path $NuGetKeyVaultSignToolPath)) {
+        New-Item $nstDir -ItemType Directory -ErrorAction Ignore | Out-Null
+        Invoke-WebRequest https://github.com/onovotny/NuGetKeyVaultSignTool/releases/download/v1.1.4/NuGetKeyVaultSignTool.1.1.4.nupkg `
+            -OutFile "$nstDir/NuGetKeyVaultSignTool.zip"
+        Expand-Archive "$nstDir/NuGetKeyVaultSignTool.zip" -DestinationPath $nstDir
+    }
+
+    $MSBuildArgs += '-p:CodeSign=true'
+    $MSBuildArgs += "-p:AzureSignToolPath=$AzureSignToolPath"
+    $MSBuildArgs += "-p:NuGetKeyVaultSignToolPath=$NuGetKeyVaultSignToolPath"
 }
 
 $artifacts = "$PSScriptRoot/artifacts/"
