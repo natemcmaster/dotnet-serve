b7d3e03337238476a8064629556de78a6bffcc65
Andrew Lock <andrewlock.net@gmail.com>
2018-10-27 21:36:31 +0100

    Add Headers middleware and options
    Allows sending headers with the file/directory response.
    Addresses #18

---
+++ src/dotnet-serve/Headers/HeadersMiddleware.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace McMaster.DotNet.Serve.Headers
+{
+    public class HeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<HeadersMiddleware> _logger;
+        private readonly HeadersOptions _options;
+
+        public HeadersMiddleware(RequestDelegate next, IOptions<HeadersOptions> options, ILogger<HeadersMiddleware> logger)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _options = options.Value;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.Response.OnStarting(() =>
+            {
+                var headers = context.Response.Headers;
+                foreach (var headerValue in _options.Headers)
+                {
+                    _logger.LogDebug("Setting header {HeaderName}:{HeaderValue}", headerValue.Key, headerValue.Value);
+                    headers[headerValue.Key] = headerValue.Value;
+                }
+
+                return Task.CompletedTask;
+            });
+
+            await _next(context);
+        }
+    }
+}
---
+++ test/dotnet-serve.Tests/CommandLineOptionsTests.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace McMaster.DotNet.Serve.Tests
+{
+    public class CommandLineOptionsTests
+    {
+        [Fact]
+        public void SplitsHeadersCorrectly()
+        {
+            var headers = new[]
+            {
+                "X-XSS-Protection: 1; mode=block",
+                "x-frame-options:SAMEORIGIN",
+            };
+            var options = new CommandLineOptions();
+            SetPropertyValue(options, nameof(CommandLineOptions.Headers), headers);
+
+            var parsedHeaders = options.GetHeaders();
+            var expectedHeaders = new Dictionary<string, string>
+            {
+                {"X-XSS-Protection", "1; mode=block" },
+                {"x-frame-options", "SAMEORIGIN" },
+            };
+
+            Assert.Equal(expectedHeaders.Count, parsedHeaders.Count);
+
+            foreach (var expectedHeader in expectedHeaders)
+            {
+                var parsedHeader = Assert.Single(parsedHeaders, h => h.Key == expectedHeader.Key);
+                Assert.Equal(expectedHeader.Value, parsedHeader.Value);
+            }
+        }
+
+        /// <summary>
+        /// Helper method for setting the backing field for getter-only auto-properties
+        /// </summary>
+        static void SetPropertyValue(CommandLineOptions options, string propertyName, object value)
+        {
+            var type = typeof(CommandLineOptions);
+            var property = type.GetProperty(propertyName);
+
+            var backingField = type
+              .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+              .First(field =>
+                field.Attributes.HasFlag(FieldAttributes.Private) &&
+                field.Attributes.HasFlag(FieldAttributes.InitOnly) &&
+                field.CustomAttributes.Any(attr => attr.AttributeType == typeof(CompilerGeneratedAttribute)) &&
+                (field.DeclaringType == property.DeclaringType) &&
+                field.FieldType.IsAssignableFrom(property.PropertyType) &&
+                field.Name.StartsWith("<" + property.Name + ">")
+              );
+            backingField.SetValue(options, value);
+        }
+    }
+}
