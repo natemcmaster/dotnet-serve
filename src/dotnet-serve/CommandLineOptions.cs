27ab224f34987efaa8c40f4b6ffff04b58c08208
Yaser Moradi <ysmoradi@outlook.com>
2019-10-21 16:34:12 +0330

    Remove brotli option from .net core 2.1 based version of tool
    Co-Authored-By: Nate McMaster <nate.mcmaster@gmail.com>

--- src/dotnet-serve/CommandLineOptions.cs
+++ src/dotnet-serve/CommandLineOptions.cs
@@ -119,6 +119,7 @@ namespace McMaster.DotNet.Serve
         [Option("-z|--gzip", Description = "Enable gzip compression")]
         public bool UseGzip { get; }
 
+#if !NETCOREAPP2_1
         [Option("-b|--brotli", Description = "Enable brotli compression")]
         public bool UseBrotli { get; }
 
