ad5a4a90b976f4b20646e615233411d9b797b501
semyonc <semyonc@gmail.com>
2023-04-08 09:22:02 +0000

    Fixed localport listening for docker containers

--- src/dotnet-serve/SimpleServer.cs
+++ src/dotnet-serve/SimpleServer.cs
@@ -73,7 +73,7 @@ internal class SimpleServer
                     }
                     else
                     {
-                        o.ListenLocalhost(port.GetValueOrDefault(), ConfigureHttps);
+                        o.ListenAnyIP(port.GetValueOrDefault(), ConfigureHttps);
                     }
                 }
                 else
